8a1e2d5b0e6cf0e100a70edc2a41b810690b1150
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-10-04 15:04:25 +0530

    [TECH] BorderLayout [TECH] ActionListener

--- javax-swing-demo/src/main/kotlin/com/javax/swing/demo/AppFrame.kt
+++ javax-swing-demo/src/main/kotlin/com/javax/swing/demo/AppFrame.kt
@@ -1,13 +1,32 @@
 package com.javax.swing.demo
 
+import java.awt.BorderLayout
 import java.awt.Dimension
+import javax.swing.JButton
 import javax.swing.JFrame
+import javax.swing.JTextArea
 
 class AppFrame : JFrame() {
+    private val textArea: JTextArea
+    private val button: JButton
+
     init {
         title = "Hola Javax Swing Demo!"
         isVisible = true
         size = Dimension(600, 600)
         defaultCloseOperation = DISPOSE_ON_CLOSE
+
+        textArea = JTextArea()
+        button = JButton(
+            "Greet!"
+        ).apply {
+            addActionListener { actionEvent ->
+                textArea.append("Ohayo gozaimasu!\n")
+            }
+        }
+
+        layout = BorderLayout()
+        add(textArea, BorderLayout.CENTER)
+        add(button, BorderLayout.SOUTH)
     }
 }
