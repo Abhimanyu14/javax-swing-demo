e4a1d6da83d0179517551435888e74bf084193ee
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-10-04 15:56:06 +0530

    [TECH] BorderFactory

--- javax-swing-demo/src/main/kotlin/com/javax/swing/demo/FormPanel.kt
+++ javax-swing-demo/src/main/kotlin/com/javax/swing/demo/FormPanel.kt
@@ -1,11 +1,16 @@
 package com.javax.swing.demo
 
+import javax.swing.BorderFactory
 import javax.swing.JPanel
 
 class FormPanel : JPanel() {
     init {
         preferredSize = preferredSize.apply {
-            width = 20
+            width = 200
         }
+
+        val innerBorder = BorderFactory.createTitledBorder("Add Person")
+        val outerBorder = BorderFactory.createEmptyBorder(8, 8, 8, 8)
+        border = BorderFactory.createCompoundBorder(outerBorder, innerBorder)
     }
 }
