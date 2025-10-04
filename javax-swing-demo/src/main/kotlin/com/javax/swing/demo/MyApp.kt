b9aebe6524382aedf8d4b1595ea01dfecb0b06f7
Abhimanyu14 <abhimanyu.n14@gmail.com>
2025-10-04 14:46:19 +0530

    [TECH] SwingUtilities

--- javax-swing-demo/src/main/kotlin/com/javax/swing/demo/MyApp.kt
+++ javax-swing-demo/src/main/kotlin/com/javax/swing/demo/MyApp.kt
@@ -2,12 +2,15 @@ package com.javax.swing.demo
 
 import java.awt.Dimension
 import javax.swing.JFrame
+import javax.swing.SwingUtilities
 
 fun main() {
-    JFrame().apply {
-        title = "Hola Javax Swing Demo!"
-        isVisible = true
-        size = Dimension(600, 600)
-        defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+    SwingUtilities.invokeLater {
+        JFrame().apply {
+            title = "Hola Javax Swing Demo!"
+            isVisible = true
+            size = Dimension(600, 600)
+            defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+        }
     }
 }
